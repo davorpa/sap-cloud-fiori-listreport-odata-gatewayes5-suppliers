86130fb2673360f379fa44e3e3ba15c461c50834
davorpa <katelme@gmail.com>
2019-11-06 09:18:43 +0000

    Initial commit from SAP Fiori List Report Application template

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("SAPGatewayES5FioriListReportAppSupplierData.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
